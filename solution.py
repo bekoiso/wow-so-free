a47c504bc8fff7c45c2bc76f31793c3e85736e92
bekoiso <zbqjfls3733@gmail.com>
2024-06-04 12:44:52 +0900

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,7 @@
 import requests
 
-from_currency = str(
-    input("Enter in the currency you'd like to convert from: ")).upper()
-
-to_currency = str(
-    input("Enter in the currency you'd like to convert to: ")).upper()
-
-amount = float(input("Enter in the amount of money: "))
-
-response = requests.get(
-    f"https://api.frankfurter.app/latest?amount={amount}&from={from_currency}&to={to_currency}")
-
-print(
-    f"{amount} {from_currency} is {response.json()['rates'][to_currency]} {to_currency}")
+def get_rate(from_currency,to_currency,amount):
+    fromupper = str(from_currency).upper()
+    toupper = str(to_currency).upper()
+    resp = requests.get(f"https://api.frankfurter.app/latest?amount={amount}&from={fromupper}&to={toupper}")
+    return resp.json()['rates'][toupper]
